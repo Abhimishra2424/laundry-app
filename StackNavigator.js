e0e49ae02654a1ce89da092d1c3cd2c6e1f6ff82
Abhishek Mishra <47156246+Abhimishra2424@users.noreply.github.com>
2023-04-09 19:08:54 +0530

    added CartScreen with fake data

--- StackNavigator.js
+++ StackNavigator.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import PickUpScreen from './screens/PickUpScreen';
+import CartScreen from './screens/CartScreen';
 
 const StackNavigator = () => {
     const Stack = createNativeStackNavigator();
@@ -12,6 +13,7 @@ const StackNavigator = () => {
             <Stack.Navigator>
                 <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
                 <Stack.Screen name="PickUp" component={PickUpScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="Cart"  component={CartScreen} options={{ headerShown: false }} />
             </Stack.Navigator>
         </NavigationContainer>
     )
